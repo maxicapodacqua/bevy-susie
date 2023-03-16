b6c6ea11477d674fa5189291c944b1d13e53143f
maxicapodacqua <kapomaxi1@gmail.com>
2023-03-15 22:16:41 -0500

    Loading assets and base game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use bevy::{prelude::*, window::PrimaryWindow};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(spawn_camera)
+    .add_startup_system(spawn_susie)
+    .run();
+}
+
+pub fn spawn_camera(
+    mut commands: Commands,
+    window_q: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_q.get_single().unwrap();
+    commands.spawn(Camera2dBundle{
+        transform: Transform::from_xyz(window.width()/2.0, window.height()/2.0, 0.0),
+        ..default()
+    });
+}
+
+
+
+pub const PLAYER_SPRITE_SIZE: f32 = 80.0;
+#[derive(Component)]
+pub struct Susie {}
+pub fn spawn_susie(
+    mut commands: Commands,
+    window_q: Query<&Window, With<PrimaryWindow>>,
+    assest_server: Res<AssetServer>
+) {
+    let window = window_q.get_single().unwrap();
+    commands.spawn((
+        SpriteBundle{
+            transform: Transform::from_xyz(window.width()/2.0, PLAYER_SPRITE_SIZE/2.0, 0.0),
+            texture: assest_server.load("sprites/adventurer_back.png"),
+            ..default()
+        },
+        Susie{}
+    ));
 }
